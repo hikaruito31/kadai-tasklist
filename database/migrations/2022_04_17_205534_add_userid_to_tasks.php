8c457f75fa0be1ca2c63c5852999e426da44e8fb
hikaru.ito <itohikarun@gmail.com>
2022-04-17 12:01:50 +0000

    teisei

--- database/migrations/2022_04_17_205534_add_userid_to_tasks.php
+++ database/migrations/2022_04_17_205534_add_userid_to_tasks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUseridToTasksTable extends Migration
+class AddUseridToTasks extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddUseridToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+             $table->unsignedBigInteger('user_id');
             
             //外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
@@ -30,6 +30,7 @@ class AddUseridToTasksTable extends Migration
     {
         Schema::table('tasks', function (Blueprint $table) {
             $table->dropColumn('user_id');
+            $table->dropForeign(['user_id']);
         });
     }
 }
